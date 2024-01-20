1508afc08be3e4b5b90e6602adcb5374754082de
gwiyeomgo <gwiyeomgo@gmail.com>
2024-01-20 14:33:03 +0900

    #56 디자인 정리하고 스토리북 수정

--- src/stories/templates/Timelines.stories.tsx
+++ src/stories/templates/Timelines.stories.tsx
@@ -2,39 +2,39 @@ import type { Meta, StoryObj } from '@storybook/react';
 import { Timelines } from '../../lib';
 
 const data = [
-  {
-    id: 1,
-    url: '/test/2020',
-    tag: '2020',
-    title: '입사',
-    contents: '2020년 ** 에 입사했습니다.',
-  },
-  {
-    id: 2,
-    url: '/test/2021',
-    tag: '2021',
-    title: '1년차 회고',
-    contents: '1년차 개발자가 되었습니다.',
-  },
-  {
-    id: 3,
-    url: '/test/2022',
-    tag: '2022',
-    title: '꾸준하게 공부 하자!!!',
-    contents: '배울게 너무 많다...',
-  },
+    {
+        id: 1,
+        url: '/test/2020',
+        tag: '2020',
+        title: '입사',
+        contents: '2020년 ** 에 입사했습니다.',
+    },
+    {
+        id: 2,
+        url: '/test/2021',
+        tag: '2021',
+        title: '1년차 회고',
+        contents: '1년차 개발자가 되었습니다.',
+    },
+    {
+        id: 3,
+        url: '/test/2022',
+        tag: '2022',
+        title: '꾸준하게 공부 하자!!!',
+        contents: '배울게 너무 많다...',
+    },
 ];
 const meta = {
-  title: '4.Templates/TimeLines',
-  component: Timelines,
-  tags: ['autodocs'],
+    title: '4.Templates/TimeLines',
+    component: Timelines,
+    tags: ['autodocs'],
 } satisfies Meta<typeof Timelines>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
-  args: {
-    data: data,
-  },
+    args: {
+        data: data,
+    },
 };
--- src/types/images.d.ts
+++ src/types/images.d.ts
@@ -1,12 +1,13 @@
+
+
+declare module '*.png' {
+  const value: string;
+  export default value;
+}
 /**
  https://stackoverflow.com/questions/44717164/unable-to-import-svg-files-in-typescript
  */
 declare module '*.svg' {
   const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
   export default content;
-}
-
-declare module '*.png' {
-  const value: string;
-  export default value;
 }
