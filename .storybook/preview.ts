d06112bb5a8f911a748caae3a6c5e1df04921f07
gwiyeomgo <gwiyeomgo@gmail.com>
2024-01-25 13:02:08 +0900

    ga4 적용4

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,12 @@
 import type { Preview } from '@storybook/react';
+import ReactGA from "react-ga4";
+
+ReactGA.initialize('G-1F4HHWF098', {
+  gtagOptions: {
+    debug_mode: true,
+    content_group: 'storybook'
+  }
+});
 
 const preview: Preview = {
   parameters: {
