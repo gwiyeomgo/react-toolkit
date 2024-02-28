2f5da942ab2f4f63df1ba30c1c947f2fa5b617cb
gwiyeomgo <gwiyeomgo@gmail.com>
2024-02-29 00:46:01 +0900

    #68 fileupload 오류 수정

--- src/lib/Upload/SingleUplaod.test.tsx
+++ src/lib/Upload/SingleUplaod.test.tsx
@@ -1,6 +1,6 @@
 import { render } from '@testing-library/react';
 import React from 'react';
-import { File, SingleUpload } from './SingleUplaod';
+import { File, SingleUpload } from './singleUplaod';
 
 it('return when targetItem is null', async () => {
   const save = async (image: File) => {
